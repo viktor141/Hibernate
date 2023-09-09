82e5c7f66c81db8d2cbc41a1c87ca65511ca154f
viktor141 <31136336+viktor141@users.noreply.github.com>
2023-09-09 16:14:10 +0400

    forgotten controller

--- src/main/java/ru/vixtor/hebernate/controller/HibernateController.java
+++ src/main/java/ru/vixtor/hebernate/controller/HibernateController.java
@@ -1,5 +1,9 @@
 package ru.vixtor.hebernate.controller;
 
+import jakarta.annotation.security.RolesAllowed;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,19 +21,28 @@ public class HibernateController {
         this.repository = repository;
     }
 
+    @Secured("ROLE_USER")
     @GetMapping("/persons/by-city")
     public List<Person> getlistPersonInCity(@RequestParam String city) {
         return repository.findByCityOfLiving(city);
     }
 
+    @RolesAllowed("ROLE_STAFF")
     @GetMapping("/persons/by-age")
-    public List<Person> getlistPersonInCity(@RequestParam int age) {
+    public List<Person> getlistPersonInAge(@RequestParam int age) {
         return repository.findByAgeLessThanOrderByAge(age);
     }
 
+    @PreAuthorize("hasRole('ROLE_STAFF') or hasRole('ROLE_ADMIN')")
     @GetMapping("/persons/by-name-surname")
-    public List<Person> getlistPersonInCity(@RequestParam String name, String surname) {
+    public List<Person> getlistPersonInNameAndSurname(@RequestParam String name, String surname) {
         return repository.findByNameAndSurname(name, surname);
+    }
+
+    @PostAuthorize("#name == authentication.principal.username")
+    @GetMapping("/persons/by-name")
+    public String getlistPersonInName(@RequestParam String name) {
+        return "Hello from secured app, " + name;
     }
 
 }
