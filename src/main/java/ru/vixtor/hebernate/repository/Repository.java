5a91c12a102e750cfdca380d7c567922958a32b1
viktor141 <31136336+viktor141@users.noreply.github.com>
2023-09-09 15:52:55 +0400

    jpa-repository-query

--- src/main/java/ru/vixtor/hebernate/repository/Repository.java
+++ src/main/java/ru/vixtor/hebernate/repository/Repository.java
@@ -1,6 +1,7 @@
 package ru.vixtor.hebernate.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.vixtor.hebernate.entity.Person;
 import ru.vixtor.hebernate.entity.PersonIDClass;
 
@@ -9,10 +10,13 @@ import java.util.List;
 @org.springframework.stereotype.Repository
 public interface  Repository extends JpaRepository<Person, PersonIDClass> {
 
+    @Query(value = "from Person where cityOfLiving =?1")
     List<Person> findByCityOfLiving(String city);
 
+    @Query(value = "from Person where age <= ?1")
     List<Person> findByAgeLessThanOrderByAge(int age);
 
+    @Query(value = "from Person where name = ?1 and surname = ?2")
     List<Person> findByNameAndSurname(String name, String surname);
 
 }
